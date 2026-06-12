f2f4d87ee7c9b7902abad76f9960ea96b8423af4
Yamil Barbeito <yamilbarbeito10@gmail.com>
2026-06-12 18:23:47 -0300

    Update BarbeitoYamilTemaC.java

--- src/main/java/com/mycompany/barbeitoyamil/temac/BarbeitoYamilTemaC.java
+++ src/main/java/com/mycompany/barbeitoyamil/temac/BarbeitoYamilTemaC.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.barbeitoyamil.temac;
 
-/**
- *
- * @author Administrator
- */
 public class BarbeitoYamilTemaC {
 
     public static void main(String[] args) {
