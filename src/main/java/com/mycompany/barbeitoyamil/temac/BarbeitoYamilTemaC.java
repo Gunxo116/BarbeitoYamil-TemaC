a38e99974d55c3e315993f172cec05076d1ac8a7
Yamil Barbeito <yamilbarbeito10@gmail.com>
2026-06-12 18:22:07 -0300

    Initial commit

---
+++ src/main/java/com/mycompany/barbeitoyamil/temac/BarbeitoYamilTemaC.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.barbeitoyamil.temac;
+
+/**
+ *
+ * @author Administrator
+ */
+public class BarbeitoYamilTemaC {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
